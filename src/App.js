154453959d34d5b40d50204033787515d6db4efe
Azhar Mahmood <mywritingfrenzy@gmail.com>
2024-09-29 16:27:46 +0530

    Learned Animation of complete page transition using framer motion

--- src/App.js
+++ src/App.js
@@ -1,41 +1,45 @@
-import React from "react";
+import React from 'react';
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 //Pages
-import Home from "./pages/home";
-import Model from "./pages/model";
+import Home from './pages/home';
+import Model from './pages/model';
 //components
-import Header from "./components/header";
+import Header from './components/header';
 //Styles
-import "./App.scss";
+import './App.scss';
+import { AnimatePresence } from 'framer-motion';
 
 function App() {
-  const imageDetails = {
-    width: 524,
-    height: 650,
-  };
+	const imageDetails = {
+		width: 254,
+		height: 320,
+	};
 
-  return (
-    <Router>
-      <Header />
-      <Route
-        render={({ location }) => (
-          <Switch location={location} key={location.pathname}>
-            <Route
-              exact
-              path='/'
-              render={() => <Home imageDetails={imageDetails} />}
-            />
-            <Route
-              exact
-              path='/model/:id'
-              render={() => <Model imageDetails={imageDetails} />}
-            />
-          </Switch>
-        )}
-      />
-    </Router>
-  );
+	return (
+		<Router>
+			<Header />
+
+			<Route
+				render={({ location }) => (
+					<AnimatePresence initial={false} exitBeforeEnter>
+						<Switch location={location} key={location.pathname}>
+							<Route
+								exact
+								path='/'
+								render={() => <Home imageDetails={imageDetails} />}
+							/>
+							<Route
+								exact
+								path='/model/:id'
+								render={() => <Model imageDetails={imageDetails} />}
+							/>
+						</Switch>
+					</AnimatePresence>
+				)}
+			/>
+		</Router>
+	);
 }
 
 export default App;
--- src/pages/model.js
+++ src/pages/model.js
@@ -1,79 +1,176 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 //Components
-import ScrollForMore from "../components/scrollForMore";
+import ScrollForMore from '../components/scrollForMore';
 //Ease
 
-const Model = () => {
-  return (
-    <div className='single'>
-      <div className='container fluid'>
-        <div className='row center top-row'>
-          <div className='top'>
-            <div className='details'>
-              <div className='location'>
-                <span>28.538336</span>
-                <span>-81.379234</span>
-              </div>
-              <div className='mua'>MUA: @mylifeascrystall</div>
-            </div>
-            <div className='model'>
-              <span className='first'>
-                <span>Y</span>
-                <span>a</span>
-                <span>s</span>
-                <span>m</span>
-                <span>e</span>
-                <span>e</span>
-                <span>n</span>
-              </span>
-              <span className='last'>
-                <span>T</span>
-                <span>a</span>
-                <span>r</span>
-                <span>i</span>
-                <span>q</span>
-              </span>
-            </div>
-          </div>
-        </div>
-        <div className='row bottom-row'>
-          <div className='bottom'>
-            <div className='image-container-single'>
-              <div className='thumbnail-single'>
-                <div className='frame-single'>
-                  <img src={require("../images/yasmeen.webp")} alt='an image' />
-                </div>
-              </div>
-            </div>
-          </div>
-          <ScrollForMore />
-        </div>
-      </div>
-      <div className='detailed-information'>
-        <div className='container'>
-          <div className='row'>
-            <h2 className='title'>
-              The insiration behind the artwork & <br /> what it means.
-            </h2>
-            <p>
-              Contrary to popular belief, Lorem Ipsum is not simply random text.
-              It has roots in a piece of classical Latin literature from 45 BC,
-              making it over 2000 years old. Richard McClintock, a Latin
-              professor at Hampden-Sydney College in Virginia, looked up one of
-              the more obscure Latin words, consectetur, from a Lorem Ipsum
-              passage, and going through the cites of the word in classical
-              literature, discovered the undoubtable source. Lorem Ipsum comes
-              from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et
-              Malorum" (The Extremes of Good and Evil) by Cicero, written in 45
-              BC. This book is a treatise on the theory of ethics, very popular
-              during the Renaissance. The first line of Lorem Ipsum, "Lorem
-              ipsum dolor sit amet..", comes from a line in section 1.10.32.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+import { motion, useTransform, useViewportScroll } from 'framer-motion';
+
+const transition = {
+	duration: 1.5,
+	ease: [0.43, 0.13, 0.23, 0.96],
+};
+
+const firstName = {
+	initial: { y: 0 },
+	animate: {
+		transition: {
+			delayChildren: 0.6,
+			staggerChildren: 0.04,
+			staggerDirection: 1,
+		},
+	},
+};
+
+const lastName = {
+	initial: { y: 0 },
+	animate: {
+		transition: {
+			delayChildren: 0.6,
+			staggerChildren: 0.04,
+			staggerDirection: -1,
+		},
+	},
+};
+
+const letter = {
+	initial: { y: 400 },
+	animate: {
+		y: 0,
+		transition: {
+			duration: 1,
+			...transition,
+		},
+	},
+};
+
+const Model = ({ imageDetails }) => {
+	const { scrollYProgress } = useViewportScroll();
+	const scale = useTransform(scrollYProgress, [0, 1], [1.1, 1.25]);
+	// const opacity = useTransform(scrollYProgress, [0, 1], [1, 0]);
+	// const y = useTransform(scrollYProgress, [0, 0.5], [0, 300]);
+	// const x = useTransform(scrollYProgress, [0, 0.5], [0, 200]);
+
+	const [canScroll, setCanScroll] = useState(false);
+
+	useEffect(() => {
+		if (canScroll === false) {
+			document.querySelector('body').classList.add('no-scroll');
+		} else {
+			document.querySelector('body').classList.remove('no-scroll');
+		}
+	}, [canScroll]);
+
+	return (
+		<motion.div
+			onAnimationComplete={() => setCanScroll(true)}
+			initial='initial'
+			animate='animate'
+			exit='exit'
+			className='single'
+		>
+			<div className='container fluid'>
+				<div className='row center top-row'>
+					<div className='top'>
+						<motion.div
+							initial={{ opacity: 0, y: 20 }}
+							animate={{
+								opacity: 1,
+								y: 0,
+								transition: { delay: 1.2, ...transition },
+							}}
+							className='details'
+						>
+							<div className='location'>
+								<span>28.538336</span>
+								<span>-81.379234</span>
+							</div>
+							<div className='mua'>MUA: @mylifeascrystall</div>
+						</motion.div>
+						<motion.div className='model'>
+							<motion.span variants={firstName} className='first'>
+								<motion.span variants={letter}>Y</motion.span>
+								<motion.span variants={letter}>a</motion.span>
+								<motion.span variants={letter}>s</motion.span>
+								<motion.span variants={letter}>m</motion.span>
+								<motion.span variants={letter}>e</motion.span>
+								<motion.span variants={letter}>e</motion.span>
+								<motion.span variants={letter}>n</motion.span>
+							</motion.span>
+							<motion.span variants={lastName} className='last'>
+								<motion.span variants={letter}>T</motion.span>
+								<motion.span variants={letter}>a</motion.span>
+								<motion.span variants={letter}>r</motion.span>
+								<motion.span variants={letter}>i</motion.span>
+								<motion.span variants={letter}>q</motion.span>
+							</motion.span>
+						</motion.div>
+					</div>
+				</div>
+				<div className='row bottom-row'>
+					<div className='bottom'>
+						<div className='image-container-single'>
+							<motion.div
+								initial={{
+									y: '-50%',
+									width: imageDetails.width,
+									height: imageDetails.height,
+								}}
+								animate={{
+									y: 0,
+									width: '100%',
+									height: window.innerWidth > 1440 ? 600 : 450,
+									transition: { delay: 0.2, ...transition },
+								}}
+								className='thumbnail-single'
+							>
+								<div className='frame-single'>
+									<motion.img
+										style={{ scale }}
+										initial={{
+											scale: 1.1,
+										}}
+										animate={{
+											transition: {
+												delay: 0.2,
+												...{ duration: 1.5, ease: [0.43, 0.13, 0.23, 0.96] },
+											},
+											y: window.innerWidth > 1440 ? -600 : -450,
+										}}
+										src={require('../images/yasmeen.webp')}
+										alt='an image'
+									/>
+								</div>
+							</motion.div>
+						</div>
+					</div>
+					<ScrollForMore />
+				</div>
+			</div>
+			<div className='detailed-information'>
+				<div className='container'>
+					<div className='row'>
+						<h2 className='title'>
+							The insiration behind the artwork & <br /> what it means.
+						</h2>
+						<p>
+							Contrary to popular belief, Lorem Ipsum is not simply random text.
+							It has roots in a piece of classical Latin literature from 45 BC,
+							making it over 2000 years old. Richard McClintock, a Latin
+							professor at Hampden-Sydney College in Virginia, looked up one of
+							the more obscure Latin words, consectetur, from a Lorem Ipsum
+							passage, and going through the cites of the word in classical
+							literature, discovered the undoubtable source. Lorem Ipsum comes
+							from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et
+							Malorum" (The Extremes of Good and Evil) by Cicero, written in 45
+							BC. This book is a treatise on the theory of ethics, very popular
+							during the Renaissance. The first line of Lorem Ipsum, "Lorem
+							ipsum dolor sit amet..", comes from a line in section 1.10.32.
+						</p>
+					</div>
+				</div>
+			</div>
+		</motion.div>
+	);
 };
 
 export default Model;
